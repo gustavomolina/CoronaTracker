35b2c548ed53d00d3058303ef0a8bf0352fb7839
ECDESENVOLVIMENTO02 <molx321@homtail.com>
2020-12-23 23:42:37 -0300

    (feat) Template and communication with covid-api

---
+++ src/test/java/io/molina/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.molina.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
